b0ee2416bf811f7382d086c03803ad32063cafca
VictorMota10 <victors_mota2018@hotmail.com>
2023-03-16 13:55:16 -0300

    port change

--- server.ts
+++ server.ts
@@ -58,6 +58,6 @@ io.on("connection", (socket: any) => {
   // });
 });
 
-server.listen(3000, () => {
-  console.log(`listening on port ${3000}`);
+server.listen(4000, () => {
+  console.log(`listening on port ${4000}`);
 });
